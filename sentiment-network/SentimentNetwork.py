8cb1679101720898f63d0b6405ed6059d9d8919d
QIAN Weishuo <koyo922@qq.com>
2017-04-16 23:14:38 +0800

    MOD: done with mini project 6

--- sentiment-network/SentimentNetwork.py
+++ sentiment-network/SentimentNetwork.py
@@ -1,3 +1,5 @@
+# %pdb
+
 # TODO: -Copy the SentimentNetwork class from Project 4 lesson
 #       -Modify it according to the above instructions
 
@@ -6,13 +8,15 @@ import sys
 
 import numpy as np
 import itertools
+from collections import defaultdict, Counter
 
 
 # Encapsulate our neural network in a class
 # noinspection PyAttributeOutsideInit,PyMethodMayBeStatic,PyShadowingNames
 # noinspection PyTypeChecker,PyRedundantParentheses,PyPep8
 class SentimentNetwork:
-    def __init__(self, reviews, labels, hidden_nodes=10, learning_rate=0.1):
+    def __init__(self, reviews, labels, hidden_nodes=10, learning_rate=0.1,
+                 min_count=50, polarity_cutoff=0.4):
         """Create a SentimentNetwork with the given settings
         Args:
             reviews(list) - List of reviews used for training
@@ -22,19 +26,37 @@ class SentimentNetwork:
 
         """
         np.random.seed(1)
-        self.pre_process_data(reviews, labels)
+        self.pre_process_data(reviews, labels, min_count, polarity_cutoff)
         self.init_network(len(self.review_vocab), hidden_nodes, 1, learning_rate)
 
-    def pre_process_data(self, reviews, labels):
+    def pre_process_data(self, reviews, labels, min_count, polarity_cutoff):
+        positive_counter = Counter()
+        negative_counter = Counter()
+        total_counter = Counter()
+        for review, label in zip(reviews, labels):
+            if label == 'POSITIVE':
+                for w in review.split(' '): positive_counter[w] += 1
+            else:
+                for w in review.split(' '): negative_counter[w] += 1
+            for w in review.split(' '): total_counter[w] += 1
+        pos_neg_ratio = defaultdict(float)
+        for w, c in total_counter.items():
+            r = positive_counter[w] / (negative_counter[w] + 1)
+            pos_neg_ratio[w] = np.log(r) if r > 1 else -np.log(1 / (r + 0.001))
+
         review_vocab = set(itertools.chain.from_iterable(r.split(' ') for r in reviews))
-        self.review_vocab = list(review_vocab)
+        self.review_vocab = [w for w in review_vocab \
+                             if total_counter[w] >= min_count \
+                             and abs(pos_neg_ratio[w]) >= polarity_cutoff]
+
         label_vocab = set(labels)
         self.label_vocab = list(label_vocab)
 
         self.review_vocab_size = len(self.review_vocab)
         self.label_vocab_size = len(self.label_vocab)
-        self.word2index = {word: idx for idx, word in enumerate(review_vocab)}
-        self.label2index = {label: idx for idx, label in enumerate(label_vocab)}
+        # 注意: 现在的self.review_vocab 跟直接的review_vocab已经不同了，滤过了一些词
+        self.word2index = {word: idx for idx, word in enumerate(self.review_vocab)}
+        self.label2index = {label: idx for idx, label in enumerate(self.label_vocab)}
 
     def init_network(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         self.input_nodes = input_nodes
@@ -67,14 +89,19 @@ class SentimentNetwork:
         assert (len(training_reviews_raw) == len(training_labels))
         training_reviews = []
         for r in training_reviews_raw:
-            training_reviews.append(np.unique([self.word2index[w] for w in r.split(' ') if w in self.word2index]))
+            wordIdxes = [self.word2index[w] for w in r.split(' ') if w in self.word2index]
+            training_reviews.append(np.unique(wordIdxes))
         correct_so_far = 0
         start = time.time()
 
         for i in range(len(training_reviews)):
             review, label = training_reviews[i], training_labels[i]
-            self.layer_1 = np.sum([self.weights_0_1[wordIdx] for wordIdx in review],
-                                  axis=0, keepdims=True)
+            # 注意,这种写法兼容空的 review
+            # np.unique()出来的结果,默认还是np.float64类型的dtype
+            # 而在ndarray的下标中,必须用int类型的dtype；例如
+            # self.weights_0_1[review.astype(int)]
+            # 有值的还好,自动识别成int；而当array([])时,就用默认的float64了,所以必须astype(int)
+            self.layer_1 = self.weights_0_1[review.astype(int)].sum(axis=0, keepdims=True)
             layer_2 = self.sigmoid(np.dot(self.layer_1, self.weights_1_2))
 
             # 注意 layer2_error是loss关于layer2的偏导,而不是L本身
@@ -90,8 +117,7 @@ class SentimentNetwork:
             self.weights_1_2 -= self.learning_rate * np.dot(self.layer_1.T, layer2_delta)
             # self.weights_0_1 -= self.learning_rate * np.dot(self.layer_0.T, layer1_delta)
             # 注意 layer1_delta[0] 里面的下标0不能省,否则维度不对
-            for wordIdx in review:
-                self.weights_0_1[wordIdx] -= self.learning_rate * layer1_delta[0]
+            self.weights_0_1[review.astype(int)] -= self.learning_rate * layer1_delta[0]
 
             if np.abs(layer2_error) < 0.5:
                 correct_so_far += 1
@@ -131,9 +157,11 @@ class SentimentNetwork:
         """
         Returns a POSITIVE or NEGATIVE prediction for the given review.
         """
-        review = np.unique([self.word2index[w] for w in review.split(' ') if w in self.word2index])
+        wordIdxes = [self.word2index[w] for w in review.split(' ') if w in self.word2index]
+        review = np.unique(wordIdxes)
 
-        self.layer_1 = np.sum(self.weights_0_1[wordIdx] for wordIdx in review)
+        # 注意,这种写法兼容空的 review
+        self.layer_1 = self.weights_0_1[review.astype(int)].sum(axis=0, keepdims=True)
         self.layer_2 = self.sigmoid(np.dot(self.layer_1, self.weights_1_2))
 
         return 'POSITIVE' if self.layer_2 >= 0.5 else 'NEGATIVE'
@@ -148,7 +176,15 @@ if __name__ == '__main__':
     labels = list(map(lambda x: x[:-1].upper(), g.readlines()))
     g.close()
 
-    # learning_rate 要慢慢试,刚开始用0.1/0.001 都不收敛
-    mlp = SentimentNetwork(reviews[:-1000], labels[:-1000], learning_rate=0.001)
-    # mlp.test(reviews[-1000:], labels[-1000:])
+    # # learning_rate 要慢慢试,刚开始用0.1/0.001 都不收敛
+    # mlp = SentimentNetwork(reviews[:-1000], labels[:-1000], learning_rate=0.001)
+    # # mlp.test(reviews[-1000:], labels[-1000:])
+    # mlp.train(reviews[:-1000], labels[:-1000])
+
+    # mlp = SentimentNetwork(reviews[:-1000],labels[:-1000],min_count=20,polarity_cutoff=0.05,learning_rate=0.01)
+    # mlp.train(reviews[:-1000],labels[:-1000])
+
+    mlp = SentimentNetwork(reviews[:-1000], labels[:-1000], min_count=20, polarity_cutoff=0.8, learning_rate=0.01)
     mlp.train(reviews[:-1000], labels[:-1000])
+
+    mlp.test(reviews[-1000:], labels[-1000:])
